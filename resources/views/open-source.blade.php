799d3c9975b6e25b8b2d38e88edbcaacf6d7c328
Mark <mtownsend5512@gmail.com>
2020-01-30 15:43:38 -0600

    Update open-source.blade.php

--- resources/views/open-source.blade.php
+++ resources/views/open-source.blade.php
@@ -9,6 +9,10 @@
 
 	<h2 class="text-2xl font-bold mb-5">Open source projects</h2>
 
+	<blockquote class="text-gray-700 text-xl bg-indigo-200 border-l-4 border-indigo-700 pl-8 py-4 rounded mb-8 italic">
+		"Kindness, like a boomerage, always returns."
+	</blockquote>
+
 	@livewire('packages', $packages)
 @endsection
 
