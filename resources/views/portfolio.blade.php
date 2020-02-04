825b947b9afa8c3d848336866ff99a5185de1e77
Mark <mtownsend5512@gmail.com>
2020-02-04 12:15:18 -0600

    portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -5,9 +5,9 @@
 
 	<h2 class="text-2xl font-bold mb-5">Here's a few examples of my work</h2>
 
-	<div class="mb-6">
+	<div class="mb-24">
 		<div class="flex items-center mb-4">
-			<div class="bg-black p-3 rounded-lg inline-block shadow">
+			<div class="bg-black p-3 rounded-lg inline-block shadow border border-black">
 				<img class="block w-10 h-10" src="{{ asset('img/dh-logo.svg') }}" alt="Dying Horizon" title="Dying Horizon">
 			</div>
 			<span class="block font-bold pl-4 text-2xl">Dying Horizon</span>
@@ -25,7 +25,29 @@
 			<li>Intercooler.js</li>
 		</ul>
 		<p>
-			<a target="_blank" href="http://dyinghorizon.com/">Check it out &rarr;</a>
+			<a target="_blank" href="http://dyinghorizon.com?ref=marktownsend.rocks">Check it out &rarr;</a>
+		</p>
+	</div>
+
+	<div class="mb-24">
+		<div class="flex items-center mb-4">
+			<div class="bg-green-200 p-3 rounded-lg inline-block shadow border border-green-400">
+				<img class="block w-10 h-10" src="{{ asset('img/rc-church.png') }}" alt="">
+			</div>
+			<span class="block font-bold pl-4 text-2xl">Rabon Chapel Church of Christ</span>
+		</div>
+		<p>
+			When I went to work designing and creating the Rabon Chapel Church of Christ site the goal was minimalism with a strong emphasis on a clean mobile experience. Traditional contact methods are reduced in favor of a more modern live chat approach of communication.
+		</p>
+		<span class="block text-sm text-gray-600 leading-none uppercase font-bold tracking-wide pb-3">Some of what I used...</span>
+		<ul class="mb-6">
+			<li>Laravel</li>
+			<li>Tailwind CSS</li>
+			<li>Scout (Laravel Package)</li>
+			<li>Wink (Laravel Package)</li>
+		</ul>
+		<p>
+			<a target="_blank" href="https://rabonchapelcoc.com?ref=marktownsend.rocks">Check it out &rarr;</a>
 		</p>
 	</div>
 @endsection
