ab61144bcd1de0f461e722523322c070071cdeab
Mark <mtownsend5512@gmail.com>
2020-02-12 10:10:03 -0600

    portfolio update

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -7,7 +7,7 @@
 
 	<div class="mb-24">
 		<div class="flex items-center mb-4">
-			<div class="bg-black p-3 rounded-lg inline-block shadow border border-black">
+			<div class="bg-black p-3 rounded-lg inline-block shadow border border-black relative">
 				<img class="block w-10 h-10" src="{{ asset('img/dh-logo.svg') }}" alt="Dying Horizon" title="Dying Horizon">
 			</div>
 			<span class="block font-bold pl-4 text-2xl">Dying Horizon</span>
@@ -31,7 +31,38 @@
 
 	<div class="mb-24">
 		<div class="flex items-center mb-4">
-			<div class="bg-green-200 p-3 rounded-lg inline-block shadow border border-green-400">
+			<div class="bg-red-700 p-3 rounded-lg inline-block shadow border border-red-800 relative">
+				<span class="text-white font-semibold text-sm tracking-wider leading-none absolute top-0 right-0 left-0 w-full text-center mt-1">HEB</span>
+				<div class="w-8 h-8 text-center relative">
+					<svg class="fill-current text-white w-7 h-6 inline-block" aria-hidden="true" data-prefix="fal" data-icon="barcode-scan" class="svg-inline--fa fa-barcode-scan fa-w-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+					  <path fill="currentColor" d="M160 8c0-4.4-3.6-8-8-8H72c-4.4 0-8 3.6-8 8v152h96V8zm128 0c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v152h64V8zm96 0c0-4.4-3.6-8-8-8h-16c-4.4 0-8 3.6-8 8v152h32V8zm96 0c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v152h64V8zm96 0c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v152h64V8zM416 504c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V352h-64v152zm-64 0c0 4.4 3.6 8 8 8h16c4.4 0 8-3.6 8-8V352h-32v152zm-288 0c0 4.4 3.6 8 8 8h80c4.4 0 8-3.6 8-8V352H64v152zm160 0c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V352h-64v152zm288 0c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V352h-64v152zm120-264H8c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h624c4.4 0 8-3.6 8-8v-16c0-4.4-3.6-8-8-8z"/>
+					</svg>
+				</div>
+			</div>
+			<span class="block font-bold pl-4 text-2xl">H-E-B barcode scanner web app</span>
+		</div>
+		<p>
+			I built this web app solely to solve first-world problems for my wife. She loves knowing the total before checkout because she's a frugal shopper, but ordering online causes you to miss out on so many recipe ideas.
+		</p>
+		<p>
+			I was able to access H-E-B's private API to retrieve product data. This app mostly uses vanilla Javascript, but sprinkles in a tiny bit of PHP for the API calls to overcome CORs issues.
+		</p>
+
+		<span class="block text-sm text-gray-600 leading-none uppercase font-bold tracking-wide pb-3">Some of what I used...</span>
+		<ul class="mb-6">
+			<li>Vanilla Javascript</li>
+			<li>Quagga.js (barcode scanning)</li>
+			<li>Tailwind CSS</li>
+			<li>H-E-B's API</li>
+		</ul>
+		<p>
+			<a target="_blank" href="https://dev.to/mtownsend5512/learning-javascript-barcode-scanning-and-wiggling-into-private-grocery-store-apis-30bh">Read more about it &rarr;</a>
+		</p>
+	</div>
+
+	<div class="mb-24">
+		<div class="flex items-center mb-4">
+			<div class="bg-green-200 p-3 rounded-lg inline-block shadow border border-green-400 relative">
 				<img class="block w-10 h-10" src="{{ asset('img/rc-church.png') }}" alt="">
 			</div>
 			<span class="block font-bold pl-4 text-2xl">Rabon Chapel Church of Christ</span>
