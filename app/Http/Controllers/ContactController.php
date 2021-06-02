c2b825225257666595461122e4125397198c64b6
Mark <mtownsend5512@gmail.com>
2021-06-02 09:42:21 -0500

    Updated contact form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ContactRequest;
 use App\Mail\ContactFormEmail;
 use Illuminate\Http\Request;
 use Mail;
+use Zttp\Zttp;
 
 class ContactController extends Controller
 {
@@ -16,13 +17,17 @@ class ContactController extends Controller
 
     public function send(ContactRequest $request)
     {
-        $data = [
-            'name' => $request->input('name'),
-            'body' => strip_tags($request->input('message')),
-            'reply_to' => trim($request->input('email'))
-        ];
+        $accessKey = 'b8c6f3f9-83e6-4659-a778-59e82b22ea32';
+        $subject = 'Contact form from marktownsend.rocks';
 
-        Mail::to(env('PERSONAL_EMAIL_ADDRESS'))->send(new ContactFormEmail($data));
+        $response = Zttp::asFormParams()->post('https://api.staticforms.xyz/submit', [
+            'accessKey' => $accessKey,
+            'name' => $request->name,
+            'email' => $request->email,
+            'replyTo' => $request->email,
+            'message' => $request->message,
+            'subject' => $subject
+        ]);
 
         return redirect()->back()->with('message', '<i class="fas fa-paper-plane text-3xl lg:text-lg md:text-lg mb-4 lg:mb-0 mr-4"></i> Thank you! Your message has been sent. I\'ll be in touch.');
     }
